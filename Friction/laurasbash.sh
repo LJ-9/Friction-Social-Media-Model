94ab7bf32de4aaf31ad30929a1add348f51e7f30
LJ-9 <laurajahn@outlook.de>
2022-12-30 11:11:39 +0100

    fix sim number from 1 to 10, per 5 nw makes for 50 runs per combi

--- Friction/laurasbash.sh
+++ Friction/laurasbash.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-user=jahnla@iu.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=25
+#SBATCH --cpus-per-task=40
 #SBATCH --time=3-23:59:00
 #SBATCH --mail-type=FAIL,BEGIN,END
 #SBATCH --job-name=finalrun
@@ -15,4 +15,4 @@ conda activate snakemake
 ######  Job commands go below this line #####
 cd /N/u/jahnla/Quartz/marketplace/Friction
 echo '###### RUNDec28 ######'
-snakemake --nolock --cores 25
+snakemake --nolock --cores 40
